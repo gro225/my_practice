786e8d4c2ed7b7ee68b22ea2c103bd1ba49264a1
Антон Воробьев <antonvorobyev2004@yandex.ru>
2024-09-14 13:14:56 +0300

    basic work of ls with all flags

---
+++ Lab1/main.c
@@ -0,0 +1,124 @@
+#include <stdio.h>
+#include <unistd.h>
+
+#include <sys/stat.h> 
+// #include <sys/types.h>
+#include <dirent.h>
+
+#include <errno.h>
+#include <string.h>
+#include <time.h>
+
+#include <pwd.h>
+#include <grp.h>
+
+void print_file_permissions(mode_t mode) {
+    char permissions[11];
+
+    // Первый символ указывает тип файла
+    if (S_ISDIR(mode)) {
+        permissions[0] = 'd';
+    } else if (S_ISREG(mode)) {
+        permissions[0] = '-';
+    } else if (S_ISLNK(mode)) {
+        permissions[0] = 'l';
+    }
+
+    // Права пользователя
+    permissions[1] = (mode & S_IRUSR) ? 'r' : '-';
+    permissions[2] = (mode & S_IWUSR) ? 'w' : '-';
+    permissions[3] = (mode & S_IXUSR) ? 'x' : '-';
+
+    // Права группы
+    permissions[4] = (mode & S_IRGRP) ? 'r' : '-';
+    permissions[5] = (mode & S_IWGRP) ? 'w' : '-';
+    permissions[6] = (mode & S_IXGRP) ? 'x' : '-';
+
+    // Права для всех остальных
+    permissions[7] = (mode & S_IROTH) ? 'r' : '-';
+    permissions[8] = (mode & S_IWOTH) ? 'w' : '-';
+    permissions[9] = (mode & S_IXOTH) ? 'x' : '-';
+
+    permissions[10] = '\0';
+
+    printf("%s ", permissions);
+}
+
+void print_last_modified(time_t time){
+    char* date = ctime(&time);
+    date[strlen(date) - 1] = '\0';
+    //Проиводим к виду ls 
+    char* space_pos = strstr(date, " ");
+    int new_length = strlen(space_pos) - 8;
+    char new_date[14];
+    strncpy(new_date, space_pos, new_length);
+    printf("%s ", new_date);
+}
+
+int main(int argc, char** argv) {
+
+    char arg;
+    int show_all = 0, long_format = 0;
+    long long total_blocks = 0;
+
+    while ((arg = getopt(argc, argv, "al")) != -1) {
+        switch (arg) {
+            case 'a':
+            show_all = 1;
+                break;
+            case 'l': 
+            long_format = 1;
+                break;
+
+            default:
+                printf("Unknown option: %c\n", arg);
+                break;
+        }
+    }
+
+	DIR* dp = opendir("./");
+	struct dirent* ep;
+
+    // Первый проход по дирректориям для подсчета их кол-во
+    while ((ep = readdir(dp)) != NULL) {
+        if (!show_all && ep->d_name[0] == '.') {
+            continue;
+        }
+
+        struct stat file_info;
+        if (stat(ep->d_name, &file_info) == 0){
+            total_blocks += file_info.st_blocks; 
+        }
+    }
+     if (long_format) {
+         printf("total %lld\n", total_blocks / 2);
+     }
+
+    // Сбрасываем позицию 
+    rewinddir(dp);
+
+    // Второй проход по дирректориями для вывода информации
+    while ((ep = readdir(dp)) != NULL){
+        if (!show_all && ep->d_name[0] == '.') {
+            continue;
+        }
+
+        if (long_format) {
+            struct stat file_info;
+            if (stat(ep->d_name, &file_info) == 0) {
+                struct passwd *pw = getpwuid(file_info.st_uid);
+                struct group  *gr = getgrgid(file_info.st_gid);
+
+                print_file_permissions(file_info.st_mode);
+                printf("%ld ", (long)file_info.st_nlink); 
+                printf("%s %s ", pw->pw_name, gr->gr_name);  
+                printf("%ld ", file_info.st_size);
+                print_last_modified(file_info.st_mtime); 
+            }
+        }
+        printf("%s\n", ep->d_name);
+
+	}
+    closedir(dp);
+    return 0;
+}
