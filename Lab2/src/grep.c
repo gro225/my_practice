0a6190eb26b129deafe5143d52fe3baf047137a0
Антон Воробьев <antonvorobyev2004@yandex.ru>
2024-10-03 23:05:18 +0300

    working with |

--- Lab2/src/grep.c
+++ Lab2/src/grep.c
@@ -5,48 +5,63 @@
 #define RED "\033[31m"
 #define RESET "\033[0m"
 
+void print_from_stream(FILE *stream, regex_t *regex){
+    char line[256];
+	const char *ptr;
+    regmatch_t match;
+
+    while (fgets(line, sizeof(line), stream) != NULL){
+        ptr = line;
+        if (regexec(regex, ptr, 1, &match, 0) == 0){
+            while (regexec(regex, ptr, 1, &match, 0) == 0){
+                fwrite(ptr, 1, match.rm_so, stdout);
+
+                printf(RED);
+                fwrite(ptr + match.rm_so, 1, match.rm_eo - match.rm_so, stdout);
+                printf(RESET);
+
+                ptr += match.rm_eo;
+            }
+
+            printf("%s", ptr);
+        }
+    }
+}
+	
+
 int main(int argc, char **argv) {
-	if (argc < 3) {
+	if (argc < 2) {
         printf("Usage: %s <pattern> <file1> [file2 ...]\n", argv[0]);
         return 1;
     }
 
-	char line[256];
-	regex_t regex;
+    regex_t regex;
 
-	int ret = regcomp(&regex, argv[1], REG_EXTENDED);
+    int ret = regcomp(&regex, argv[1], REG_EXTENDED);
     if (ret) {
         printf("Could not compile regex\n");
         return 1;
     }
 
-	for(int i = 2; i < argc; i++) {
-		FILE *fp = fopen(argv[i], "r");
-    	if (fp == NULL) {
-        	printf("grep_utilite: %s: No such file or directory\n", argv[i]);
-            continue;
-		}
-
-		while (fgets(line, sizeof(line), fp) != NULL){
-            const char *ptr = line;
-            regmatch_t match;
-            if (regexec(&regex, ptr, 1, &match, 0) == 0){
-                while (regexec(&regex, ptr, 1, &match, 0) == 0){
-                    fwrite(ptr, 1, match.rm_so, stdout);
-
-                    printf(RED);
-                    fwrite(ptr + match.rm_so, 1, match.rm_eo - match.rm_so, stdout);
-                    printf(RESET);
-
-                    ptr += match.rm_eo;
-                }
-
-                printf("%s", ptr);
-            }
+    if(argc > 2){
+        for(int i = 2; i < argc; i++) {
+		    FILE *fp = fopen(argv[i], "r");
+    	    if (fp == NULL) {
+        	    printf("grep_utilite: %s: No such file or directory\n", argv[i]);
+                continue;
+		    }
+            print_from_stream(fp, &regex);
+            fclose(fp);
         }
-		fclose(fp);
-	}
+    }
+    else{
+        print_from_stream(stdin, &regex);
+    }
+
     regfree(&regex);
+
     return 0;
 }
-  
+
+
+           
