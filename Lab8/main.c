cf9afe2e725994c79c31a8d7f97a8a4b6c685905
Антон Воробьев <antonvorobyev2004@yandex.ru>
2024-12-20 14:31:50 +0300

    перепутал с 10

--- Lab8/main.c
+++ Lab8/main.c
@@ -8,20 +8,18 @@
 
 char shared_array[ARRAY_SIZE + 1];  
 pthread_mutex_t mutex;              // Мьютекс для синхронизации
-pthread_cond_t cond;                
 int counter = 0;                    
 int current_tid = 0;                
 
 // Функция пишущего потока
 void* writer_thread() {
     while (1) {
-        pthread_mutex_lock(&mutex);
+        pthread_mutex_lock(&mutex);  
         snprintf(shared_array, ARRAY_SIZE + 1, "%010d", counter++);
 
-        current_tid = 0;  
-        pthread_cond_broadcast(&cond);  
+        current_tid = 0;
 
-        pthread_mutex_unlock(&mutex);
+        pthread_mutex_unlock(&mutex);  
         sleep(WRITE_INTERVAL); 
     }
     return NULL;
@@ -32,22 +30,20 @@ void* reader_thread(void* arg) {
     int tid = *(int*)arg;
 
     while (1) {
-        pthread_mutex_lock(&mutex);
+        while (1) {
+            pthread_mutex_lock(&mutex);  
 
+            if (tid == current_tid) {
+                printf("Reader TID %d: [%s]\n", tid, shared_array);
+                current_tid++;  
 
+                pthread_mutex_unlock(&mutex); 
+                break;  
+            }
 
-        while (tid != current_tid) {
-            pthread_cond_wait(&cond, &mutex);
+            pthread_mutex_unlock(&mutex);  
+            usleep(1000);  
         }
-
-
-        printf("Reader TID %d: [%s]\n", tid, shared_array);
-
-        
-        current_tid++;
-        pthread_cond_broadcast(&cond);
-
-        pthread_mutex_unlock(&mutex);
     }
     return NULL;
 }
@@ -58,25 +54,21 @@ int main() {
     int tids[NUM_READERS];
 
     pthread_mutex_init(&mutex, NULL);
-    pthread_cond_init(&cond, NULL);
 
     pthread_create(&writer, NULL, writer_thread, NULL);
 
-   
     for (int i = 0; i < NUM_READERS; i++) {
         tids[i] = i;
         pthread_create(&readers[i], NULL, reader_thread, &tids[i]);
     }
 
-
     pthread_join(writer, NULL);
     for (int i = 0; i < NUM_READERS; i++) {
         pthread_join(readers[i], NULL);
     }
-
-    // Освобождение ресурсов
+    
+    //Очистка
     pthread_mutex_destroy(&mutex);
-    pthread_cond_destroy(&cond);
 
     return 0;
 }
